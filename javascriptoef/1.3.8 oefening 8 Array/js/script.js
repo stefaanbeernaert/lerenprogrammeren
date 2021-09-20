c4c1d7a57d807ef42acc1bacd9fe254219e989a0
stefaan beernaert <beernaertstefaan@gmail.com>
2021-09-20 20:31:27 +0200

    oef 8

--- javascriptoef/1.3.8 oefening 8 Array/js/script.js
+++ javascriptoef/1.3.8 oefening 8 Array/js/script.js
@@ -1,2 +1,15 @@
 var aantal = parseInt(prompt("Hoeveel Namen wenst u in te geven"));
-var namen = [];
+var namen = []
+
+
+//test foreach loop met array
+// namen.forEach(element => console.log(element));
+
+for (var i = 1;i <= aantal;i++){
+   var naam = prompt("geef een naam");
+   namen.push(naam);
+}
+namen.sort();
+
+
+console.log(namen);
